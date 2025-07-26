b7917763404331ff0e58634eb65d2738b3a407b4
Marc Gueury <marc.gueury@oracle.com>
2025-07-26 08:52:16 +0200

    v3.0

--- starter/src/terraform/genai_kb_23ai.tf
+++ starter/src/terraform/genai_kb_23ai.tf
@@ -106,7 +106,8 @@ output "private_endpoint_d" {
 
 locals {
   # Replace the hostname with the IP in the connection string (else KB creation will give Internal Server Error)
-  db_url_ip = replace(local.db_url, data.oci_database_autonomous_database.starter_atp.private_endpoint, data.oci_database_autonomous_database.starter_atp.private_endpoint_ip)
+  db_url_ip1 = replace(local.db_url, data.oci_database_autonomous_database.starter_atp.private_endpoint, data.oci_database_autonomous_database.starter_atp.private_endpoint_ip)
+  db_url_ip = replace(local.db_url_ip1, "retry_count=20", "retry_count=3")
 }
 
 output "db_url_ip" {
@@ -125,6 +126,10 @@ resource "oci_database_tools_database_tools_connection" "starter_dbtools_connect
     # The user password to use exists as a secret in an OCI Vault
     secret_id  = oci_vault_secret.starter_secret_atp.id
   }
+  related_resource {
+    entity_type = "DATABASE"
+    identifier  = oci_database_autonomous_database.starter_atp.id
+  }  
   private_endpoint_id = oci_database_tools_database_tools_private_endpoint.starter_database_tools_private_endpoint.id
 }
 
