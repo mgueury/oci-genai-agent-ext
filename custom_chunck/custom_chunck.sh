811f152d5b4f5af9a3d55ca34f79b384d040185d
Marc Gueury <marc.gueury@oracle.com>
2025-07-28 17:56:16 +0200

    v3.0

--- custom_chunck/custom_chunck.sh
+++ custom_chunck/custom_chunck.sh
@@ -2,15 +2,9 @@
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 cd $SCRIPT_DIR/..
 
-rm -r starter/src/db/*
-rm starter/src/terraform/psql.tf
 cp -r oracle/* starter/.
 
-sed -i 's/TF_VAR_db_type="psql"/TF_VAR_db_type="autonomous"/' starter/env.sh
-sed -i 's/TF_VAR_prefix="psql"/TF_VAR_prefix="db23ai"/' starter/env.sh
-sed -i 's/TF_VAR_db_user="postgres"/TF_VAR_db_user="admin"/' starter/env.sh
-sed -i 's/POSTGRES/DB23ai/' starter/src/compute/app/requirements.txt
-sed -i 's/psycopg2-binary/oracledb/' starter/src/compute/app/requirements.txt
-sed -i '/searchActionJS.js/a \
-sed -i "s/PostgreSQL/Oracle DB 23ai/" webApps/search_ui/flows/simple/pages/input-search-page.html \
-' starter/src/ui/build_ui.sh
+# sed -i 's/TF_VAR_prefix="psql"/TF_VAR_prefix="db23ai"/' starter/env.sh
+# sed -i 's/TF_VAR_db_user="postgres"/TF_VAR_db_user="admin"/' starter/env.sh
+# sed -i 's/POSTGRES/DB23ai/' starter/src/compute/app/requirements.txt
+# sed -i 's/psycopg2-binary/oracledb/' starter/src/compute/app/requirements.txt
