56f22c02ace08246e1d95a7fab272e96284d6eef
Marc Gueury <marc.gueury@oracle.com>
2025-09-11 12:23:27 +0200

    v3.0

--- starter/src/terraform/genai_kb_23ai.tf
+++ starter/src/terraform/genai_kb_23ai.tf
@@ -106,7 +106,7 @@ data "oci_database_tools_database_tools_private_endpoint" "starter_database_tool
 
 locals {
   # Replace the hostname with the IP in the connection string (else KB creation will give Internal Server Error)
-  db_url_ip1 = replace(local.db_url, data.oci_database_autonomous_database.starter_atp.private_endpoint, data.oci_database_autonomous_database.starter_atp.private_endpoint_ip)
+  db_url_ip1 = replace(local.local_db_url, data.oci_database_autonomous_database.starter_atp.private_endpoint, data.oci_database_autonomous_database.starter_atp.private_endpoint_ip)
   db_url_ip = replace(local.db_url_ip1, "retry_count=20", "retry_count=3")
 }
 
