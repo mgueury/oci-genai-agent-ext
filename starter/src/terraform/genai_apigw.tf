c74d00baecd52a502d1d968160392112359864c6
Marc Gueury <marc.gueury@oracle.com>
2025-09-11 12:22:24 +0200

    v3.0

--- starter/src/terraform/genai_apigw.tf
+++ starter/src/terraform/genai_apigw.tf
@@ -108,7 +108,7 @@ resource "oci_apigateway_deployment" "starter_apigw_deployment_ords" {
       methods = [ "ANY" ]
       backend {
         type = "HTTP_BACKEND"
-        url    = "${local.local_db_root_url}/ords/$${request.path[pathname]}"
+        url    = "${local.db_root_url}/ords/$${request.path[pathname]}"
         connect_timeout_in_seconds = 60
         read_timeout_in_seconds = 120
         send_timeout_in_seconds = 120            
