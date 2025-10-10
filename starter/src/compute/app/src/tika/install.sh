54622bda4e19c7ea9592c73ac5e5349cbf17bd8b
Marc Gueury <marc.gueury@oracle.com>
2025-10-10 20:10:39 +0200

    v3.0

---
+++ starter/src/compute/app/src/tika/install.sh
@@ -0,0 +1,9 @@
+# Java
+sudo dnf install -y graalvm-25-jdk
+sudo update-alternatives --set java /usr/lib64/graalvm/graalvm-java25/bin/java
+echo "export JAVA_HOME=/usr/lib64/graalvm/graalvm-java25" >> $HOME/.bashrc
+
+# Maven
+sudo dnf install -y maven
+
+mvn build
