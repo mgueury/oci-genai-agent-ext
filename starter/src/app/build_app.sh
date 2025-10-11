7f52deb93995b7ca78d148ca865d93b154838125
Marc Gueury <marc.gueury@oracle.com>
2025-10-11 15:34:38 +0200

    v3.0

--- starter/src/app/build_app.sh
+++ starter/src/app/build_app.sh
@@ -11,39 +11,6 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 . $SCRIPT_DIR/../../starter.sh env -no-auto
 . $BIN_DIR/build_common.sh
 
-
-## XXXXX Check Language version
-get_attribute_from_tfstate "STREAM_OCID" "starter_stream" "id"
-get_attribute_from_tfstate "TENANCY_NAME" "tenant_details" "name"
-get_attribute_from_tfstate "STREAM_MESSAGE_ENDPOINT" "starter_stream" "messages_endpoint"
-
-get_id_from_tfstate "TF_VAR_agent_datasource_ocid" "starter_agent_ds" 
-get_id_from_tfstate "TF_VAR_agent_endpoint_ocid" "starter_agent_endpoint" 
-
-get_id_from_tfstate "APP_SUBNET_OCID" "starter_app_subnet" 
-get_id_from_tfstate "DB_SUBNET_OCID" "starter_db_subnet" 
-
-oci generative-ai model-collection list-models --compartment-id $TF_VAR_compartment_ocid --all > $TARGET_DIR/genai_models.json 
-export TF_VAR_genai_meta_model=$(jq -r '.data.items[]|select(.vendor=="meta" and (.capabilities|index("CHAT")))|.["display-name"]' $TARGET_DIR/genai_models.json | head -n 1)
-echo $TF_VAR_genai_meta_model
-
-export TF_VAR_genai_cohere_model=$(jq -r '.data.items[]|select(.vendor=="cohere" and (.capabilities|index("CHAT")))|.["display-name"]' $TARGET_DIR/genai_models.json | head -n 1)
-echo $TF_VAR_genai_cohere_model
-
-export TF_VAR_genai_embed_model="cohere.embed-multilingual-v3.0"
-# export TF_VAR_genai_embed_model=$(jq -r '.data.items[]|select(.vendor=="cohere" and (.capabilities|index("TEXT_EMBEDDINGS")) and ."time-on-demand-retired"==null)|.["display-name"]' $TARGET_DIR/genai_models.json | head -n 1)
-echo $TF_VAR_genai_embed_model
-
-echo
-echo "-- STREAMING CONNECTION --------------------------"
-echo "STREAM_MESSAGE_ENDPOINT=$STREAM_MESSAGE_ENDPOINT"
-echo "STREAM_OCID=$STREAM_OCID"
-echo "STREAM_USERNAME=$TENANCY_NAME/$TF_VAR_username/$STREAM_OCID"
-echo
-echo "-- AGENT (OPTIONAL) ---------------------------"
-echo "TF_VAR_agent_datasource_ocid=$TF_VAR_agent_datasource_ocid"
-echo "TF_VAR_agent_endpoint_ocid=$TF_VAR_agent_endpoint_ocid"
-
 if is_deploy_compute; then
   mkdir -p ../../target/compute/$APP_DIR
   cp -r src/* ../../target/compute/$APP_DIR/.
