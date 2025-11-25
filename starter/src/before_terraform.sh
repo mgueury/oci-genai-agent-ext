d5f6f686b657b8abf537d2881be7fa5dc44702cc
Marc Gueury <marc.gueury@oracle.com>
2025-11-25 09:35:37 +0100

    v3.0

--- starter/src/before_terraform.sh
+++ starter/src/before_terraform.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 if [ "$TF_VAR_rag_storage" == "db26ai" ]; then
-  # Use the KB of 23ai 
-  if [ -f src/terraform/genai_kb_23ai._tf ]; then
-    echo "Setting RAG Storage 23ai"
-    mv src/terraform/genai_kb_23ai._tf src/terraform/genai_kb_23ai.tf
+  # Use the KB of 26ai 
+  if [ -f src/terraform/genai_kb_26ai._tf ]; then
+    echo "Setting RAG Storage db26ai"
+    mv src/terraform/genai_kb_26ai._tf src/terraform/genai_kb_26ai.tf
     mv src/terraform/genai_kb_os.tf src/terraform/genai_kb_os._tf
   fi
 fi
