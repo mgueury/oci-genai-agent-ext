fde1b9752f44f3c95375bb4b171d2446c1aa454f
Marc Gueury <marc.gueury@oracle.com>
2025-12-07 18:52:00 +0100

    v3.0

--- starter/src/terraform/network.tf
+++ starter/src/terraform/network.tf
@@ -257,6 +257,18 @@ resource "oci_core_security_list" "starter_security_list" {
     }
   }  
 
+  // Langgraph
+  ingress_security_rules {
+    protocol  = "6" // tcp
+    source    = local.cidr_vcn
+    stateless = false
+
+    tcp_options {
+      min = 2024
+      max = 2024
+    }
+  }  
+
   freeform_tags = local.freeform_tags
 }
 
