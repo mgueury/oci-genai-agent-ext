74e0e7c38902112ee63e622762cbb7e2d58b787a
Marc Gueury <marc.gueury@oracle.com>
2026-01-03 02:00:56 +0100

    v3.0

--- starter/src/terraform/openid.tf
+++ starter/src/terraform/openid.tf
@@ -90,7 +90,7 @@ resource "oci_apigateway_deployment" "starter_apigw_deployment-openid" {
       methods = [ "GET" ]
       backend {
         type = "OAUTH2_LOGOUT_BACKEND"
-        allowed_post_logout_uris = [ "/logout_html", "https://www.oracle.com", "https://${local.apigw_hostname}/" ]
+        allowed_post_logout_uris = [ "/logout_html", "https://www.oracle.com", "https://${local.apigw_hostname}/", "https://${local.apigw_hostname}/openid/chat.html" ]
       }
     }
     # https://xxxxxx.apigateway.eu-frankfurt-1.oci.customer-oci.com/openid/logout?postLogoutUrl=/logout_html
