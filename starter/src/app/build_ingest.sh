54b8a365df9ec96099b19afd2da942ddd1e0eedd
Marc Gueury <marc.gueury@oracle.com>
2026-01-08 20:02:36 +0100

    v3.0

--- starter/src/app/build_ingest.sh
+++ starter/src/app/build_ingest.sh
@@ -13,6 +13,7 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 if is_deploy_compute; then
   build_rsync $APP_SRC_DIR
 else
+  cp ../../bin/shared_compute.sh $APP_SRC_DIR
   docker image rm ${TF_VAR_prefix}-${APP_NAME}:latest
   docker build -f Dockerfile_${APP_NAME} -t ${TF_VAR_prefix}-${APP_NAME}:latest .
   exit_on_error "docker build"
