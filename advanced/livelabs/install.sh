1375c342321257998ba74fd40b2e94b3167a01e6
Marc Gueury <marc.gueury@oracle.com>
2026-01-29 18:32:34 +0100

    v3.0

--- advanced/livelabs/install.sh
+++ advanced/livelabs/install.sh
@@ -2,10 +2,11 @@
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 cd $SCRIPT_DIR
 
-cd ../starter
+cd ../../starter
 
-cp ../advanced/* .
+cp ../advanced/livelabs/* .
 rm install.sh
-mv ssrc/apigw.tf src/apigw._tf 
+mv src/apigw.tf src/apigw._tf 
 mv src/genai_apigw.tf src/genai_apigw._tf 
-sed -i 's/oci_apigateway_/# oci_apigateway_" "kubernetes"/' src/terraform/build.tf 
+sed -i 's/oci_apigateway_/# oci_apigateway_/' src/terraform/build.tf 
+
